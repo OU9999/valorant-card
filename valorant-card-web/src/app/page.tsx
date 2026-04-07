4db5afbf791998d86e5baf7d88c1720f75427d72
오유진 <omh232323@gmail.com>
2026-04-07 15:46:40 +0900

    refactor: 히어로 섹션 쇼케이스 데이터 mock-data로 통합 및 Suspense 제거

--- valorant-card-web/src/app/page.tsx
+++ valorant-card-web/src/app/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import { HeroSection } from "@/components/home/hero-section";
 
 export default function Home() {
-  return (
-    <Suspense>
-      <HeroSection />
-    </Suspense>
-  );
+  return <HeroSection />;
 }
